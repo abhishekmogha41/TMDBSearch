798c8612b09f4413d900a57ba090f743437adcbf
abhishekmogha41 <57898407+abhishekmogha41@users.noreply.github.com>
2023-01-27 12:35:03 +0530

    banner

--- src/neflix/Banner.js
+++ src/neflix/Banner.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import request from "./request";
 
 function Banner() {
+  const [bannerMovie, setBannerMobie] = useState("");
+  const img_base_path = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
-    const result = axios.get(request.fetchNetflixOriginals);
-    console.log(result);
+    const fetchData = async () => {
+      const results = await axios.get(request.fetchNetflixOriginals);
+      console.log(results);
+      setBannerMobie(
+        results.data.results[
+          Math.floor(Math.random() * results.data.results.length)
+        ]
+      );
+    };
+    fetchData();
   }, []);
   return (
     <>
-      <div className="banner">
-        <h1></h1>
+      <div
+        className="banner"
+        style={{
+          backgroundImage:
+            "url(" + img_base_path + bannerMovie.backdrop_path + ")",
+        }}
+      >
+        <h1>
+          {bannerMovie.title || bannerMovie.name || bannerMovie.original_name}
+        </h1>
         <div className="cta">
-          <a href=""></a>
-          <a href=""></a>
-          <p></p>
+          <a href="">Play</a>
+          <a href="">My List</a>
+          <p>{bannerMovie.overview}</p>
         </div>
       </div>
     </>
